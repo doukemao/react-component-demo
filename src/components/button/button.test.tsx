451ebeb7d461a70ef038f6800af328303e2c214b
doukemao <415270363@qq.com>
2021-11-30 17:03:57 +0800

    add stylelint

--- src/components/button/button.test.tsx
+++ src/components/button/button.test.tsx
@@ -4,7 +4,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import Button from "./Button";
+import Button from "./button";
 
 describe("button", () => {
   it("renders the Button component", () => {
---
+++ stylelint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": ["stylelint-config-standard", "stylelint-config-standard-scss"],
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "rules": {
+    // recommended rules
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    // any other rules you'd want to change e.g.
+  }
+};
